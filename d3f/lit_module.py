c3221a932e3a7bb2fb8b6df1cd4ea17fda6348a7
Thomas Rowntree <thomas.james.rowntree@gmail.com>
2022-08-01 21:13:42 +0930

    add rotation and remove some noise and distortion. Much better context but worst domain transfer

--- d3f/lit_module.py
+++ d3f/lit_module.py
@@ -73,27 +73,34 @@ class LitModule(pl.LightningModule):
             # A.RandomRotate90(),
             # A.Flip(),
             # A.Transpose(),
-            A.OneOf([
-                # A.IAAAdditiveGaussianNoise(),
-                A.GaussNoise(),
-            ], p=0.2),
+            # A.OneOf([
+            #     # A.IAAAdditiveGaussianNoise(),
+            #     A.GaussNoise(),
+            # ], p=0.2),
             A.OneOf([
                 A.MotionBlur(p=.2),
                 A.MedianBlur(blur_limit=3, p=0.1),
                 A.Blur(blur_limit=3, p=0.1),
             ], p=0.2),
-            A.ShiftScaleRotate(shift_limit=0.0, scale_limit=0.2, rotate_limit=10, p=0.2),
-            A.OneOf([
-                A.OpticalDistortion(p=0.3),
-                A.GridDistortion(p=.1),
-                # A.IAAPiecewiseAffine(p=0.3),
-            ], p=0.2),
+            A.ShiftScaleRotate(
+                shift_limit=0.1,
+                scale_limit=0.25,
+                rotate_limit=10,
+                border_mode=cv2.BORDER_CONSTANT,
+                value=0.0,
+                p=0.5,
+            ),
+            # A.OneOf([
+            #     A.OpticalDistortion(p=0.3),
+            #     A.GridDistortion(p=.1),
+            #     # A.IAAPiecewiseAffine(p=0.3),
+            # ], p=0.2),
             A.OneOf([
                 A.CLAHE(clip_limit=2),
 
                 A.RandomBrightnessContrast(),            
             ], p=0.3),
-            A.HueSaturationValue(hue_shift_limit=60,p=0.1),
+            # A.HueSaturationValue(hue_shift_limit=60,p=0.1),
             A.Normalize(mean,std,max_pixel_value=1.0),
             ToTensorV2(),
         ])
